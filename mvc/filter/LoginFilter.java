69d9f4bbb0955e0b966ede39a5da8bcc35833b69
okita-souji <54254266+okita-souji@users.noreply.github.com>
2020-05-15 11:02:20 +0800

    Add files via upload

---
+++ mvc/filter/LoginFilter.java
@@ -0,0 +1,47 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpServletResponse res = (HttpServletResponse)response;
+		HttpSession session = req.getSession(true);
+		
+		String userName = (String) session.getAttribute("userName");		
+		
+		if (userName==null || "".equals(userName)) {
+			res.sendRedirect(req.getContextPath()+"/login.jsp");
+		}
+		else {
+			chain.doFilter(request, response);
+		}
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
